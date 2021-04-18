0df8161d4f4b823360ddd2d1d1de924fd3b028be
Dario Segura <darionco.dev@gmail.com>
2021-04-18 10:59:22 -0400

    [CLIENT] Added loading animation

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,18 +1,39 @@
 import {GraferView} from './garfer/view';
 import {TwitterView} from './twitter/view';
 
+function createLoading(container): HTMLElement {
+    const el = document.createElement('div');
+    el.className = 'data-loading';
+
+    const spinner = document.createElement('div');
+    spinner.className = 'lds-dual-ring';
+
+    const label = document.createElement('div');
+    label.className = 'data-loading-label';
+    label.innerText = 'LOADING...';
+
+    el.appendChild(spinner);
+    el.appendChild(label);
+    container.appendChild(el);
+    return el;
+}
+
 async function main(): Promise<void> {
     const pathName = window.location.pathname;
     const pathComponents = pathName.split('/').filter(v => Boolean(v));
     const dataPack = pathComponents.pop() || 'adam_inferred';
 
     const grafer = new GraferView(document.body);
-    await grafer.init(dataPack as any);
 
-    const twitter = new TwitterView(document.body, grafer);
+    const loading = createLoading(document.body);
 
-    grafer.on('node-clicked', (type: string, node: any) => {
-        twitter.displayTweet(node);
+    grafer.init(dataPack as any).then(() => {
+        const twitter = new TwitterView(document.body, grafer);
+        grafer.on('node-clicked', (type: string, node: any) => {
+            twitter.displayTweet(node);
+        });
+
+        loading.parentElement.removeChild(loading);
     });
 }
 
