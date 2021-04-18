a6f5c4ae1dbb2e46abb1fd305a6891c8a7b2ea36
darionco <darionco.dev@gmail.com>
2021-04-18 15:00:22 +0000

    Deploying to gh-pages from  @ 0df8161d4f4b823360ddd2d1d1de924fd3b028be 🚀

--- js/main.js
+++ js/main.js
@@ -21439,15 +21439,31 @@ var TwitterView = class {
 };
 
 // src/main.ts
+function createLoading(container) {
+  const el = document.createElement("div");
+  el.className = "data-loading";
+  const spinner = document.createElement("div");
+  spinner.className = "lds-dual-ring";
+  const label = document.createElement("div");
+  label.className = "data-loading-label";
+  label.innerText = "LOADING...";
+  el.appendChild(spinner);
+  el.appendChild(label);
+  container.appendChild(el);
+  return el;
+}
 async function main() {
   const pathName = window.location.pathname;
   const pathComponents = pathName.split("/").filter((v) => Boolean(v));
   const dataPack = pathComponents.pop() || "adam_inferred";
   const grafer = new GraferView2(document.body);
-  await grafer.init(dataPack);
-  const twitter = new TwitterView(document.body, grafer);
-  grafer.on("node-clicked", (type, node) => {
-    twitter.displayTweet(node);
+  const loading = createLoading(document.body);
+  grafer.init(dataPack).then(() => {
+    const twitter = new TwitterView(document.body, grafer);
+    grafer.on("node-clicked", (type, node) => {
+      twitter.displayTweet(node);
+    });
+    loading.parentElement.removeChild(loading);
   });
 }
 main();
