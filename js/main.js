f13fa095e28cb354de6632d178dc0f259c5becce
darionco <darionco.dev@gmail.com>
2021-04-20 22:49:34 +0000

    Deploying to gh-pages from  @ 342d2e295dbf236708dbf94403c30d9229d3311a 🚀

--- js/main.js
+++ js/main.js
@@ -21306,6 +21306,57 @@ var GraferView2 = class extends EventEmitter {
   }
 };
 
+// src/twitter/tweet.js
+var Tweet = class {
+  constructor(container, node, twttr, theme, closeCB) {
+    this.container = container;
+    this.element = document.createElement("div");
+    this.element.className = "tweet-container";
+    this.container.insertBefore(this.element, this.container.firstChild);
+    const buttons = this.makeTweetButtons(closeCB);
+    this.element.appendChild(buttons);
+    const tweetText = this.makeTweetRaw(node.tweet);
+    this.element.appendChild(tweetText);
+    twttr.widgets.createTweet(node.label, this.element, {
+      theme,
+      width: 250,
+      conversation: "none",
+      align: "center"
+    }).then((tweet) => {
+      if (tweet) {
+        this.element.removeChild(tweetText);
+      }
+    });
+  }
+  remove() {
+    this.container.removeChild(this.element);
+  }
+  makeTweetButtons(closeCB) {
+    const container = document.createElement("div");
+    container.className = "tweet-container-buttons";
+    container.textContent = "\u2716";
+    container.addEventListener("click", closeCB);
+    return container;
+  }
+  makeTweetRaw(tweet) {
+    const tweetRaw = document.createElement("div");
+    tweetRaw.className = "tweet-raw";
+    const author = document.createElement("div");
+    author.className = "tweet-raw-author";
+    author.textContent = tweet.author;
+    tweetRaw.appendChild(author);
+    const user = document.createElement("div");
+    user.className = "tweet-raw-user";
+    user.textContent = `@${tweet.user}`;
+    tweetRaw.appendChild(user);
+    const text = document.createElement("div");
+    text.className = "tweet-raw-text";
+    text.textContent = tweet.text;
+    tweetRaw.appendChild(text);
+    return tweetRaw;
+  }
+};
+
 // src/twitter/view.js
 var TwitterView = class {
   constructor(container, grafer) {
@@ -21330,30 +21381,6 @@ var TwitterView = class {
     this.linkColor = style.getPropertyValue("--tweet-to-node").trim();
     this.tweetTheme = style.getPropertyValue("--tweet-theme").trim();
     this.initializeEvents();
-  }
-  async displayTweet(node) {
-    if (this.tweets.has(node.label)) {
-      this.removeTweet(node.label);
-    } else {
-      const tweetContainer = document.createElement("div");
-      tweetContainer.className = "tweet-container";
-      this.list.insertBefore(tweetContainer, this.list.firstChild);
-      const buttons = this.makeTweetButtons(node);
-      tweetContainer.appendChild(buttons);
-      const tweetText = this.makeTweetRaw(node.tweet);
-      tweetContainer.appendChild(tweetText);
-      const point = this.grafer.getWorldPointPosition(node.point);
-      this.tweets.set(node.label, {element: tweetContainer, point});
-      const tweet = await this.twttr.widgets.createTweet(node.label, tweetContainer, {
-        theme: this.tweetTheme,
-        width: 250,
-        conversation: "none",
-        align: "center"
-      });
-      if (tweet) {
-        tweetContainer.removeChild(tweetText);
-      }
-    }
   }
   initializeEvents() {
     let animationFrame = null;
@@ -21387,9 +21414,9 @@ var TwitterView = class {
     this.context.clearRect(0, 0, size[0], size[1]);
     this.context.strokeStyle = this.linkColor;
     this.context.lineWidth = 3;
-    for (const tweet of this.tweets.values()) {
-      const bb = tweet.element.getBoundingClientRect();
-      const point = this.grafer.worldToPixel(tweet.point);
+    for (const info of this.tweets.values()) {
+      const bb = info.tweet.element.getBoundingClientRect();
+      const point = this.grafer.worldToPixel(info.point);
       let tweetY = bb.y + bb.height * 0.5;
       if (tweetY < listBB.top) {
         tweetY = listBB.top - (listBB.top - tweetY) * 0.025;
@@ -21402,16 +21429,27 @@ var TwitterView = class {
       this.context.stroke();
     }
   }
+  async displayTweet(node) {
+    if (this.tweets.has(node.label)) {
+      this.removeTweet(node.label);
+    } else {
+      const tweet = new Tweet(this.list, node, this.twttr, this.tweetTheme, () => {
+        this.removeTweet(node.label);
+      });
+      const point = this.grafer.getWorldPointPosition(node.point);
+      this.tweets.set(node.label, {tweet, point});
+    }
+  }
   removeTweet(id) {
-    const tweet = this.tweets.get(id);
-    if (tweet) {
-      tweet.element.parentElement.removeChild(tweet.element);
+    const info = this.tweets.get(id);
+    if (info) {
+      info.tweet.remove();
       this.tweets.delete(id);
     }
   }
   clearTweets() {
-    for (const tweet of this.tweets.values()) {
-      tweet.element.parentElement.removeChild(tweet.element);
+    for (const info of this.tweets.values()) {
+      info.tweet.remove();
     }
     this.tweets.clear();
   }
@@ -21440,32 +21478,6 @@ var TwitterView = class {
   }
   makeList() {
     return this.makeEmptyElement("twitter-list");
-  }
-  makeTweetButtons(tweet) {
-    const container = document.createElement("div");
-    container.className = "tweet-container-buttons";
-    container.textContent = "\u2716";
-    container.addEventListener("click", () => {
-      this.removeTweet(tweet.label);
-    });
-    return container;
-  }
-  makeTweetRaw(tweet) {
-    const tweetRaw = document.createElement("div");
-    tweetRaw.className = "tweet-raw";
-    const author = document.createElement("div");
-    author.className = "tweet-raw-author";
-    author.textContent = tweet.author;
-    tweetRaw.appendChild(author);
-    const user = document.createElement("div");
-    user.className = "tweet-raw-user";
-    user.textContent = `@${tweet.user}`;
-    tweetRaw.appendChild(user);
-    const text = document.createElement("div");
-    text.className = "tweet-raw-text";
-    text.textContent = tweet.text;
-    tweetRaw.appendChild(text);
-    return tweetRaw;
   }
 };
 
